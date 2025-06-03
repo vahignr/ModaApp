1e41140ab4e65cae72bcce06e03a56a4a402059e
vahiguner <vahiguner@ibm.com>
2025-06-03 12:59:27 -0500

    Initial Commit

---
+++ ModaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModaApp
+//
+//  Created by Vahi Guner on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ModaApp/ModaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModaAppApp.swift
+//  ModaApp
+//
+//  Created by Vahi Guner on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ModaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
